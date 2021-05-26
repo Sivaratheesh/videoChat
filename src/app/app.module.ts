924e0e0d773d1f374c4549fc9246fefd07d9d008
Siva Ratheesh <ratheesh93.ngl@gmail.com>
2021-05-26 16:45:42 +0530

    build conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { VideoService } from './video.service';
+const routes: Routes = [
+  {path:'',component:HomeComponent}];
 
 @NgModule({
   declarations: [
@@ -15,6 +18,7 @@ import { VideoService } from './video.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(routes),
     FormsModule
   ],
   providers: [VideoService],
