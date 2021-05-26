6cd6b46dff24a17d99a3cb89f6e32d4cbf32bd2e
Siva Ratheesh <ratheesh93.ngl@gmail.com>
2021-05-26 17:36:17 +0530

    resolved

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
 
   public streaming = false;
 
-  public video:any;
+  public cameraSrc:any;
   public canvas:any;
   public photo :any;
   public startbutton = null;
@@ -20,39 +20,8 @@ export class HomeComponent implements OnInit {
   constructor( public videoseervice:VideoService) { }
 
   ngOnInit(): void {
-    this.videoseervice.video = document.getElementById('video');
+    this.cameraSrc = document.getElementById('video');
+    this.videoseervice.video =  this.cameraSrc
     this.videoseervice.getvideo();
-    // this.canvas = document.getElementById('canvas');
-    // this.photo = document.getElementById('photo');
-    // this.startbutton = document.getElementById('startbutton');
-
-    // navigator.mediaDevices
-    // .getUserMedia({
-    //   video: {
-    //     facingMode: 'environment'
-    //   },audio:true
-    // })
-    // .then((stream) => {
-    //   this.video.srcObject = stream;
-    //   this.video.setAttribute('playsinline', 'true'); // required to tell iOS safari we don't want fullscreen
-    //   this.video.play();
-    //  this.video.getVideoTracks();
-    //   // let url = window.URL.createObjectURL(stream);
-    //   // console.log(url)
-    // })
-    // .catch(function(err) {
-    //     console.log("An error occurred: " + err);
-    // });
-    // this.video.addEventListener('canplay', function(ev){
-    //   if (this.streaming) {
-    //     this.height =this.video.videoHeight / (this.video.videoWidth/this.width);
-  
-    //     this.video.setAttribute('width',this. width);
-    //     this.video.setAttribute('height',this. height);
-    //     this. canvas.setAttribute('width',this. width);
-    //     this.canvas.setAttribute('height',this. height);
-    //     this.streaming = true;
-    //   }
-    // }, true);
   }
 }
