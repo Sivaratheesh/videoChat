850c1257746b2dc02ec856b7c01f2dbadf8fef69
Sivaratheesh H <sivaratheesh.h@lyncspace.com>
2022-08-31 10:56:00 +0530

    server file changes

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/webrtc'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/webrtc/'}),
-);
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/webrtc/'}),
+// );
+app.all('*', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/project-folder/index.html');
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
